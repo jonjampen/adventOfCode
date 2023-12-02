0cb4cb2e0373daaa7a51890c50ce64a5a0b39fdf
Jon Jampen <jon.jampen@cryptography.ch>
2023-12-02 11:18:26 +0100

    Adding dynamic import for solutions.

--- src/routes/[year]/[day]/+page.svelte
+++ src/routes/[year]/[day]/+page.svelte
@@ -1,5 +1,12 @@
 <script>
+	import { onMount } from "svelte";
+
 	export let data;
+	let Solution;
+	onMount(async () => {
+		Solution = (await import(`../../../solutions/${data.year}/${data.day}/Solution.svelte`)).default;
+	});
 </script>
 
 <h1 class="text-3xl">Advent of Code {data.year}: Day {data.day}</h1>
+<svelte:component this={Solution} />
